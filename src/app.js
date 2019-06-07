ce3512f5025728b64771120a09a1947840efe5f5
cgm11 <caro.garcia0506@gmail.com>
2019-06-07 06:01:19 +0200

    .

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const hbs = require('hbs');
+require('./helper')
+
+const directoriopublico = path.join(__dirname, '../public');
+const directoriopartials = path.join(__dirname, '../partials');
+
+app.use(express.static(directoriopublico));
+
+hbs.registerPartials(directoriopartials);
+
+app.set('view engine', 'hbs');
+
+app.get('/',(req, res) => {
+    res.render('index', {
+        estudiante: 'Carolina'
+    });
+});
+
+app.get('/calculos', (req, res) => {
+    console.log(req.query);
+    res.render('calculos', {
+        estudiante: req.query.nombre,
+        nota1: parseInt(req.query.nota1),
+        nota2: parseInt(req.query.nota2),
+        nota3: parseInt(req.query.nota3)
+    })
+}) 
+
+app.listen(3000, () => {
+    console.log('Escuchando en el puerto 3000');
+});
